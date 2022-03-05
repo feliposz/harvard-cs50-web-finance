94aa06be8bf0ac6dc2c019cf0820abb74386d3b8
feliposz <feliposz@gmail.com>
2022-03-05 14:36:32 -0300

    My implementation (back in 2011)

--- includes/constants.php
+++ includes/constants.php
@@ -11,16 +11,16 @@
 
 
     // your database's name (i.e., username_pset7)
-    define("DB_NAME", "");
+    define("DB_NAME", "jharvard_pset7");
 
     // your database's username
-    define("DB_USER", "");
+    define("DB_USER", "jharvard");
 
     // your database's password
-    define("DB_PASS", "");
+    define("DB_PASS", "crimson");
 
     // hostname of course's database server
-    define("DB_SERVER", "mysql.localdomain");
+    define("DB_SERVER", "localhost");
 
     // URL for Yahoo Finance
     define("YAHOO", "http://download.finance.yahoo.com/d/quotes.csv?f=snl1d1t1c1ohg&s=");
--- index.php
+++ index.php
@@ -3,6 +3,25 @@
     // require common code
     require_once("includes/common.php"); 
 
+    // SQL to get user data
+    $sql = "SELECT * FROM users WHERE uid='{$_SESSION["uid"]}'";
+
+    // get results
+    $result = mysql_query($sql);
+
+    // if we found a row, remember user and redirect to portfolio
+    if (mysql_num_rows($result) == 1)
+    {
+        // grab row
+        $row = mysql_fetch_array($result);
+        $username = $row["username"];
+        $usercash = $row["cash"];
+    }
+    else
+    {
+        apologize("Unable to get user information.");
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -21,7 +40,67 @@
     </div>
 
     <div id="middle">
-      <img alt="Under Construction" height="299" src="images/construction.gif" width="404">
+        Welcome, <?= $username ?>.<br>
+        <p>Current cash: $ <? printf("%.2f", $usercash); ?></p>
+        <p><a href="quote.php">Get quotes</a></p>
+        <p><a href="buy.php">Buy stocks</a></p>
+        <p><a href="history.php">Check history</a></p>
+        <p><a href="password.php">Change password</a></p>
+
+        <?
+        // SQL to get user portfolio listing
+        $sql = "SELECT * FROM portfolio WHERE uid='{$_SESSION["uid"]}'";
+
+        // get results
+        $result = mysql_query($sql);
+
+        // error getting results for user
+        if (!$result)
+        {
+            apologize("Unable to get portfolio information.");
+        }
+        ?>
+        
+        <? if (mysql_num_rows($result) > 0): ?>
+
+        <table class="listing">
+            <caption>Your current portfolio</caption>
+            <thead>
+                <tr>
+                    <td>Symbol</td>
+                    <td>Description</td>
+                    <td>Shares</td>
+                    <td>Value</td>
+                    <td>Operation</td>
+                </tr>
+            </thead>
+            <tbody>
+
+            <? while ($row = mysql_fetch_array($result)): ?>
+
+                <? $s = lookup($row["symbol"]); ?>
+                <tr>
+                    <td><?= $row["symbol"] ?></td>
+                    <td><?= $s->name ?></td>
+                    <td><?= $row["shares"] ?></td>
+                    <td><? printf("%.2f", $s->price); ?></td>
+                    <td>
+                        <a href="sell.php?symbol=<?= urlencode($row["symbol"]) ?>">Sell</a>
+                        <a href="buy.php?symbol=<?= urlencode($row["symbol"]) ?>">Buy</a>
+                    </td>
+                </tr>
+
+            <? endwhile ?>
+
+            </tbody>
+        </table>
+
+        <? else: ?>
+
+        <p>No portfolio.</p>
+
+        <? endif ?>
+
     </div>
 
     <div id="bottom">
